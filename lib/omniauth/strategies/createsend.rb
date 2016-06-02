883c23d3e3de50f5a521be9ed961ce6810d52254
Brandon <brandonjyi@gmail.com>
2016-06-02 16:40:45 -0700

    Fix regression in omniauth-oauth2 v1.4.0

--- lib/omniauth/strategies/createsend.rb
+++ lib/omniauth/strategies/createsend.rb
@@ -9,6 +9,12 @@ module OmniAuth
         :authorize_url => "/oauth",
         :token_url => "/oauth/token"
       }
+
+      # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+      #
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
